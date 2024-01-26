ba2104b505e70d75c2fdd08c8dd91254e061b03a
Pavel Artem'yev <pasha.artemev48@gmail.com>
2024-01-27 01:26:20 +0300

    043 Добавление сущности Review

---
+++ backend/src/main/java/com/example/bookstore/database/entity/Review.java
@@ -0,0 +1,33 @@
+package com.example.bookstore.database.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Date;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userEmail;
+
+    @CreationTimestamp
+    private Date date;
+
+    private double rating;
+
+    private Long productId;
+
+    private String reviewText;
+}

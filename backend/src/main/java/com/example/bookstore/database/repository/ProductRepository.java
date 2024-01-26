6317923158c758ae40c1b9c7dcd54f9de0ebeb3f
Pavel Artem'yev <pasha.artemev48@gmail.com>
2024-01-26 23:20:17 +0300

    033 Подготовка api для реализации поиска

--- backend/src/main/java/com/example/bookstore/database/repository/ProductRepository.java
+++ backend/src/main/java/com/example/bookstore/database/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.example.bookstore.database.repository;
 
 import com.example.bookstore.database.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<Product> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }

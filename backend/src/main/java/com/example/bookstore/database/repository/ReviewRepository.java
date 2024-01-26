932dec3e4944f73da87a8f12140360bc46e4cdcc
Pavel Artem'yev <pasha.artemev48@gmail.com>
2024-01-27 01:38:00 +0300

    044 Добавление репозитория ReviewRepository

---
+++ backend/src/main/java/com/example/bookstore/database/repository/ReviewRepository.java
@@ -0,0 +1,16 @@
+package com.example.bookstore.database.repository;
+
+import com.example.bookstore.database.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findByProductId(@RequestParam("productId") Long productId, Pageable pageable);
+
+    Review findByUserEmailAndProductId(String userMail, Long productId);
+}
